a0912d8fc51a26fcaec5819bfa81f6c7cf73e77c
tarourskyy <pchytara@gmail.com>
2021-03-17 15:48:57 +0700

    menambahkan project

---
+++ LatihanClassAndObject/Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassAndObject
+{
+    public class Mahasiswa
+    {
+        //properties
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        //method
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("NIM: {0}", Nim);
+            Console.WriteLine("IPK: {0}", Ipk);
+            Console.WriteLine("Telah Melakukan Registrasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs\n", Nama);
+        }
+    }
+}
---
+++ LatihanClassAndObject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class Mahasiswa
+            Mahasiswa mhs1 = new Mahasiswa();
+            Mahasiswa mhs2 = new Mahasiswa();
+
+            // mengatur nilai properties objek mhs1 dan mhs2
+            mhs1.Nim = "1991";
+            mhs1.Nama = "Pete";
+            mhs1.Ipk = 39.9f;
+
+            mhs2.Nim = "1993"; ;
+            mhs2.Nama = "Kao";
+            mhs2.Ipk = 4.00f;
+
+            // memanggil method registrasi dan isi krs
+            mhs1.Registrasi();
+            mhs1.IsiKrs();
+
+            mhs2.Registrasi();
+            mhs2.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
